707a8f43a32b53c81ba619dcb00d21fca513c5cf
eric-v <s1190348@student.windesheim.nl>
2024-12-11 11:32:17 +0100

    Deleted lib

--- Eros.Library/Models/Authentication/Login.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Eros.Library.Models.Authentication;
-
-public class Login
-{
-	public string? Email { get; set; }
-	public string? Password { get; set; }
-}
--- Eros.Library/Models/Authentication/Registration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Eros.Library.Models.Authentication;
-
-public class Registration : DataModel
-{
-	public string Name { get; set; } = string.Empty;
-	public string EmailAddress { get; set; } = string.Empty;
-	public string Password { get; set; } = string.Empty;
-}
