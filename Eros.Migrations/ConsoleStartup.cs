3f92d5e7864a5c0065cd8f30116959b0b9106a32
eric-v <s1190348@student.windesheim.nl>
2024-12-11 11:29:40 +0100

    Revert "deleted unused files"
    This reverts commit 54c241ab5653a9ba70cdfca1ad37c2b81912a474.

---
+++ Eros.Migrations/ConsoleStartup.cs
@@ -0,0 +1,29 @@
+﻿using Eros.API;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eros.Migrations;
+
+public class ConsoleStartup
+{
+	public ConsoleStartup()
+	{
+		var builder = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+			.AddEnvironmentVariables();
+		Configuration = builder.Build();
+	}
+
+	public IConfiguration Configuration { get; }
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+		});
+	}
+}
---
+++ ErosApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Eros.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Eros.App" };
+        }
+    }
+}
